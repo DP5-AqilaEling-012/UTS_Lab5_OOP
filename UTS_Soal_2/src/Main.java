bcea69092887b67e061b0596546e6777dc4b8c20
Aqila Eling Febrianti <114634831+DP5-AqilaEling-012@users.noreply.github.com>
2024-05-20 10:25:43 +0700

    Main.java

--- UTS_Soal_2/src/Main.java
+++ UTS_Soal_2/src/Main.java
@@ -3,22 +3,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Membuat objek Scanner untuk masukan dari pengguna
+        //Membuat objek Scanner untuk masukan dari pengguna
         Scanner input = new Scanner(System.in);
 
-        // Meminta pengguna untuk memasukkan dua string
+        //Meminta pengguna untuk memasukkan dua string
         System.out.print("Masukkan kata pertama: ");
         String str1 = input.nextLine();
         System.out.print("Masukkan kata kedua: ");
         String str2 = input.nextLine();
 
-        // Memeriksa apakah kedua string adalah anagram
+        //Memeriksa apakah kedua string adalah anagram
         boolean result = AnagramChecker.areAnagrams(str1, str2);
-
-        // Menampilkan hasil
+        
         System.out.println(result);
-
-        // Menutup scanner
         input.close();
     }
 }
