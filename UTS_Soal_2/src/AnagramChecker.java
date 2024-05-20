583b2f1f4fa1f08873efb74544cdb8ecb2d964eb
Aqila Eling Febrianti <114634831+DP5-AqilaEling-012@users.noreply.github.com>
2024-05-20 10:25:00 +0700

    AnagramChecker.java

--- UTS_Soal_2/src/AnagramChecker.java
+++ UTS_Soal_2/src/AnagramChecker.java
@@ -3,20 +3,20 @@ import java.util.Arrays;
 public class AnagramChecker {
 
     public static boolean areAnagrams(String str1, String str2) {
-        // Periksa apakah panjang kedua string sama
+        //Periksa apakah panjang kedua string sama
         if (str1.length() != str2.length()) {
             return false;
         }
 
-        // Ubah kedua string menjadi array karakter
+        //Ubah kedua string menjadi array karakter
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
 
-        // Urutkan kedua array karakter
+        //Urutkan kedua array karakter
         Arrays.sort(arr1);
         Arrays.sort(arr2);
 
-        // Periksa apakah kedua array karakter yang diurutkan identik
+        //Periksa apakah kedua array karakter yang diurutkan identik
         return Arrays.equals(arr1, arr2);
     }
 }
